b18d70e5fb32f5f3e81f3e155cf2445133b89095
alida-maryse <68588247+alida-maryse@users.noreply.github.com>
2020-10-29 15:55:29 +0100

    efface node modules

--- express_authors/index.js
+++ express_authors/index.js
@@ -1,10 +1,21 @@
+//const express = require('express');
+
+//const app = express();
 const express = require('express');
-
 const app = express();
+const port = 3000;
 
+const authors = ['Lawrence Nowell, UK', 'William Shakespeare, UK', 'Charles Dickens, US','Oscar Wilde, UK' ]
 
-app.get('/', (req, res) => {
+const books = ['Beowulf', 'Hamlet, Othello, Romeo and Juliet, MacBeth', 'Oliver Twist, A Christmas Carol', 'The Picture of Dorian Gray, The Importance of Being Earnest']
+
+/*app.get('/', (req, res) => {
     res.send(`Authors API !`);
+});*/
+
+
+app.get('/authors/:id', (req, res) => {
+  res.send(authors[req.params.id -1])
 });
 
 const port = 3000;
